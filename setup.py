960cf85b1cd6558e568310fcfdce6976a6e12253
Stephen Shank <sshank314@gmail.com>
2023-07-18 11:11:29 -0400

    Setuptools and anaconda packaging.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='assemblyqc',
     python_requires='>3.6.6',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/stephenshank/assemblyqc',
     download_url="https://github.com/stephenshank/assemblyqc/archive/v0.0.1.tar.gz",
     description='Quality control of assemblies',
@@ -14,11 +14,15 @@ setup(
     maintainer_email='sshank314@gmail.com',
     install_requires=[
         'biopython>=1.76',
+        'xmltodict==0.13.0'
     ],
     packages=['assemblyqc'],
     entry_points={
         'console_scripts': [
-            'regal = assemblyqc.regal:regal_cli'
+            'regal = assemblyqc.regal:regal_cli',
+            'dss_xml_cleaner = assemblyqc.xmlcleaner:cli',
+            'xml_to_json = assemblyqc.xmltojson:cli',
+            'dss_json_cleaner = assemblyqc.jsoncleaner:cli'
         ]
     },
     classifiers=[
